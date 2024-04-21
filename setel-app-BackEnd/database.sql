4a4734c731de744385b9720d405badaf9950aaf8
Jannatin Nurrohmah <61218518+Jannatin12@users.noreply.github.com>
2024-04-21 13:58:10 +0700

    Create Databae-Tabel Kendaraan

--- setel-app-BackEnd/database.sql
+++ setel-app-BackEnd/database.sql
@@ -25,3 +25,23 @@ CREATE TABLE IF NOT EXISTS Kendaraan (
   shelter VARCHAR(20),
   status VARCHAR(12)
 );
+
+/* Tabel Kendaraan - Inventory Page */
+CREATE TABLE IF NOT EXISTS Kendaraan (
+  idKendaraan INT AUTO_INCREMENT PRIMARY KEY,
+  nomorUnit VARCHAR(10),
+  jenisKendaraan VARCHAR(10),
+  shelter VARCHAR(20),
+  status VARCHAR(12)
+);
+
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('A001', 'Sepeda', 'TULT', 'Available');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('B002', 'Skuter', 'GKU', 'Available');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('A003', 'Sepeda', 'Open Library', 'Unavailable');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('B004', 'Skuter', 'MSU', 'Available');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('A005', 'Sepeda', 'TULT', 'Unavailable');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('B006', 'Skuter', 'GKU', 'Available');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('A007', 'Sepeda', 'Open Library', 'Available');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('B008', 'Skuter', 'MSU', 'Unavailable');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('A009', 'Sepeda', 'TULT', 'Available');
+INSERT INTO Kendaraan (nomorUnit, jenisKendaraan, shelter, status) VALUES ('B010', 'Skuter', 'GKU', 'Available');
